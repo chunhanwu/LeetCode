7606b9b03a63dc608b576615ed28f0756c254f2e
alexLiang <liang8156@gmail.com>
2018-08-18 23:37:45 +0800

    reversr integer 程式碼完成

--- number/reverse_integer/scripts/index.js
+++ number/reverse_integer/scripts/index.js
@@ -1,15 +1,31 @@
 
+let testNum = 120;
+const maxVaule = 2 ** 31 - 1;  // 2147483647
+const minVaule = -1 * 2 ** 31; //-2147483648
 
 //math
 function reverseByMath(num) {
-    let max = 2**31-1;  // 2147483647
-    let min = -1*2**31; //-2147483648
-    let rev = 0
-    while(num!==0){
+    let res = 0
+    while (num !== 0) {
         let pop = num % 10;
         num = parseInt(num / 10);
-        rev = rev * 10 + pop;
+        if (res > parseInt(maxVaule / 10) || (res === parseInt(maxVaule / 10) && pop > 7)) {
+            return 0
+        }
+        if (res < parseInt(minVaule / 10) || (res === parseInt(minVaule / 10) && pop < -8)) {
+            return 0
+        }
+        // return (res < MIN_VALUE || res > MAX_VALUE) ? 0 : res;
+        res = res * 10 + pop;
     }
-    return rev;
+    return res;
 };
-console.warn('reverseByMath(num)',reverseByMath(102))
+
+//String
+function reverseByString(num) {
+    let reversedInt = (+String(Math.abs(num)).split('').reverse().join('')) * Math.sign(num);
+    return (reversedInt < minVaule || reversedInt > maxVaule) ? 0 : reversedInt
+};
+console.warn('testNum', testNum)
+console.warn('reverseByMath(testNum)', reverseByMath(testNum))
+console.warn('reverseByString(testNum)', reverseByString(testNum))
