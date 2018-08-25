69b7ef78e5080c869899569800e6820712886c24
liang <liang8156@gmail.com>
2018-08-25 18:06:14 +0800

    程式碼待寫

--- string/longest_common_prefix/scripts/index.js
+++ string/longest_common_prefix/scripts/index.js
@@ -2,6 +2,12 @@
  * @param {string[]} strs
  * @return {string}
  */
+let input =  ['flower','flow','flight'];
 function longestCommonPrefix (strs) {
-    
+    let same = '';
+    console.warn('strs',strs)
+    return same
 };
+
+console.warn('input',input);
+console.warn('output',longestCommonPrefix(input));
